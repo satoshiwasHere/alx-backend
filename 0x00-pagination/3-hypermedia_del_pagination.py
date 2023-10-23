3025c88e97baaa8981d12ce4432474dd303f7948
satoshiwasHere <skinyanjui100@gmail.com>
2023-10-23 21:20:50 +0000

    corretion

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -1,4 +1,4 @@
-#!/usr/biin/env python3
+#!/usr/bin/env python3
 """
 if between two queries, certain rows are
 removed from the dataset, user does not miss items from
@@ -12,20 +12,17 @@ from typing import List, Dict
 
 class Server:
     """
-    Server class that paginates a database of popular baby names.
+    Server class to paginate a database of popular baby names.
     """
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
-        '''
-        Initializes instances
-        '''
         self.__dataset = None
         self.__indexed_dataset = None
 
     def dataset(self) -> List[List]:
         """
-        The cached dataset
+        The Cached dataset
         """
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
@@ -37,8 +34,7 @@ class Server:
 
     def indexed_dataset(self) -> Dict[int, List]:
         """
-        Dataset indexed by sorting position,
-        starting at 0
+        The dataset indexed by sorting position, starting at 0
         """
         if self.__indexed_dataset is None:
             dataset = self.dataset()
@@ -49,32 +45,33 @@ class Server:
         return self.__indexed_dataset
 
     def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
-        '''
-        Takes 2 integer arguments and returns a dictionary with
+        """
+        incoperates 2 integer arguments and returns a dictionary with
         the following key-value pairs:
         index - start of index of the page
         next_index - start of index of the next page
         page_size
         page_size - number of items on the page
         data - the data content in the page itself
-        '''
-        assert 0 <= index < len(self.dataset())
+        """
+        dataset = self.indexed_dataset()
+        data_length = len(dataset)
+        assert 0 <= index < data_length
+        response = {}
+        data = []
+        response['index'] = index
+        for i in range(page_size):
+            while True:
+                curr = dataset.get(index)
+                index += 1
+                if curr is not None:
+                    break
+            data.append(curr)
 
-        indexed_dataset = self.indexed_dataset()
-        indexed_page = {}
-
-        i = index
-        while (len(indexed_page) < page_size and i < len(self.dataset())):
-            if i in indexed_dataset:
-                indexed_page[i] = indexed_dataset[i]
-            i += 1
-
-        page = list(indexed_page.values())
-        page_indices = indexed_page.keys()
-
-        return {
-            'index': index,
-            'next_index': max(page_indices) + 1,
-            'page_size': len(page),
-            'data': page
-        }
+        response['data'] = data
+        response['page_size'] = len(data)
+        if dataset.get(index):
+            response['next_index'] = index
+        else:
+            response['next_index'] = None
+        return response
