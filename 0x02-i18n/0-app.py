78b701e37f109054a5d514b37043fc0df6cf088c
satoshiwasHere <skinyanjui100@gmail.com>
2023-10-31 16:22:51 +0000

    Basic Flask app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python3
+""" basic flask app
 """
-basic Flask app 
-"""
-from flask import Flask
-from routes.routes_0 import app_routes
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
 
-app.register_blueprint(app_routes)
+
+@app.route('/', strict_slashes=False)
+def index() -> str:
+    """route
+    """
+    return render_template('0-index.html')
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+    app.run(port="5000", host="0.0.0.0", debug=True)
