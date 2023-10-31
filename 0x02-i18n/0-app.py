e176029d985733cf5a3d7fc1ee910f196c9ccead
satoshiwasHere <skinyanjui100@gmail.com>
2023-10-31 16:16:42 +0000

    Basic Flask app

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+basic Flask app 
+"""
+from flask import Flask
+from routes.routes_0 import app_routes
+
+
+app = Flask(__name__)
+
+app.register_blueprint(app_routes)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
